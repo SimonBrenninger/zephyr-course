9db0fa1535d2fc9c1759b376980ace7cf3fadcc0
Simon Brenninger <simon.brenninger@pm.me>
2026-09-16 22:37:31 +0200

    register init hook via Kconfig

--- app/boards/custom_scratch/board.c
+++ app/boards/custom_scratch/board.c
@@ -1,12 +1,7 @@
-#include <zephyr/init.h>
 #include <zephyr/kernel.h>
 
 
-static int custom_scratch_init_hook(void)
+void board_late_init_hook(void)
 {
 	printk("Board Initialized\n");
-	return 0;
 }
-
-
-SYS_INIT(custom_scratch_init_hook, POST_KERNEL, 0);
